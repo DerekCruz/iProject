935e94e8cce05f29becab65a52f02a691661474d
Derek Cruz <derekcruz184@yahoo.com>
2018-07-08 14:38:42 -0700

    Changes to the ionic project

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,9 +6,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+ searchQuery:string ="";
+ item:string[];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController ) {
+        this.initializeItems();
+   }
+initializeItems(){
+  this.item = ['Apple','banana','Oranges',];
   }
 
+getItems(ev:any){
+  this.initializeItems();
+  const val = ev.target.value;
+ 
+  if(val && val.trim()!=''){
+    this.item=this.item.filter((item)=>{
+      return (item.toLowerCase().indexOf(val.toLowerCase()) >-1);   
+    })
+     }
+  }
 }
+  
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+//import { getOrCreateNodeInjectorForNode } from '@angular/core/src/render3/di';
+
+
+class info {
+  name:String; 
+  email:String;
+  content:String;
+}
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
+
 export class ContactPage {
+public newInfo:info;
+private todo : FormGroup;
 
-  constructor(public navCtrl: NavController) {
 
+  constructor(public navCtrl: NavController,private formBuilder: FormBuilder) {
+
+    this.newInfo= new info();
+
+      this.todo = this.formBuilder.group({
+        name: ['', Validators.required],
+        email: [''],
+        content:[""],
+      });
+    
+  
+    }
+    
+    sendInfo(){
+      console.log(this.newInfo);
+    
   }
-
+  logForm(){
+    console.log(this.todo.value);
+  }
 }
